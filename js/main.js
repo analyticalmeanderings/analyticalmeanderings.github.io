e83c417bbafc44750cf3033583be6d03d4c52769
Matthew Christian <matthew.christian@USP.org>
2021-09-22 10:35:41 -0400

    sdasda
    :wq~

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
+function openCity(evt, cityName) {
+    // Declare all variables
     var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
   
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
   
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
   }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
+
+  .tabcontent {
+    animation: fadeEffect 1s; /* Fading effect takes 1 second */
+  }
