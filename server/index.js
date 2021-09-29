497934106b3977da534caaa0905d7ab37151ec76
Matthew Christian <2010.mbc@gmail.com>
2021-09-29 15:42:24 -0400

    final version from github

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -7,15 +8,17 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-/* TODO: figure out why this is not working
-app.arguments(bodyParser.json({limit:"30mb", extended:true}));
-app.arguments(bodyParser.urlencoded({limit:"30mb", extended:true}));
-*/
-app.use('/post', postRoutes);
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://analyticalmeanderings:gopher@oilsupplymap.hqm30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+app.use('/posts', postRoutes);
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(()=> app.listen(PORT, ()=> console.log('Server running on port: ${PORT}')))
-    .catch((error)=> console.log(error.message));
+const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
