845e139aeb5ad8c13247556e348c84f6bb3a14c6
Matthew Christian <matthew.christian@USP.org>
2021-10-05 17:16:36 -0400

    took their code

--- script.js
+++ script.js
@@ -39,42 +39,44 @@ function openTab(evt, tabName) {
 
 function build_table(input_location, div_id_target) {
   
-  fetch(location, {credentials: 'include'})  
-    .then(function(response) {
+  fetch(input_location)
+  .then(function(response){
       return response.text();
-    })
-    .then(function(data){
-      var rows = data.trim().split(/\r?\n|\r/); // Regex to split/separate the CSV rows
-      var table = '';
-      var table_rows = '';
-      var table_header = '';
+  })
+  .then(function(data){
+      var table_container = document.getElementById(div_id_target);
+      csv_string_to_table(data, table_container);
+  });
 
-      rows.forEach(function(row, row_index) {
-        var table_columns = '';
-        var columns = row.split(','); // split/separate the columns in a row
-        columns.forEach(function(column, column_index) {
-            table_columns += row_index == 0 ? '<th>' + column + '</th>' : '<td>' + column + '</td>';
-        });
-        if (row_index == 0) {
-            table_header += '<tr>' + table_columns + '</tr>';
-        } else {
-            table_rows += '<tr>' + table_columns + '</tr>';
-        }
-    });
-  
-    table += '<table>';
-        table += '<thead>';
-            table += table_header;
-        table += '</thead>';
-        table += '<tbody>';
-            table += table_rows;
-        table += '</tbody>';
-    table += '</table>';
-  
-    
-    var div_container = document.getElementById('div_id_target');
-    div_container.innerHTML += table;
+}
 
-    });
+function csv_string_to_table(csv_string, element_to_insert_table) {
+  var rows = csv_string.trim().split(/\r?\n|\r/); // Regex to split/separate the CSV rows
+  var table = '';
+  var table_rows = '';
+  var table_header = '';
 
+  rows.forEach(function(row, row_index) {
+      var table_columns = '';
+      var columns = row.split(','); // split/separate the columns in a row
+      columns.forEach(function(column, column_index) {
+          table_columns += row_index == 0 ? '<th>' + column + '</th>' : '<td>' + column + '</td>';
+      });
+      if (row_index == 0) {
+          table_header += '<tr>' + table_columns + '</tr>';
+      } else {
+          table_rows += '<tr>' + table_columns + '</tr>';
+      }
+  });
+
+  table += '<table>';
+      table += '<thead>';
+          table += table_header;
+      table += '</thead>';
+      table += '<tbody>';
+          table += table_rows;
+      table += '</tbody>';
+  table += '</table>';
+
+  element_to_insert_table.innerHTML += table;
 }
