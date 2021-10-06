b8d2344dd1a763a86d91be71aa994bce7388bb00
Matthew Christian <matthew.christian@USP.org>
2021-10-05 23:42:45 -0400

    sankey

--- script.js
+++ script.js
@@ -158,3 +158,28 @@ function build_alerts(){
   //   console.error(err);
   // });
 }
+
+anychart.onDocumentReady(function(){
+  //creating the data
+  var data = [
+  {from: "Google", to: "Facebook", weight: 20000},
+  {from: "Google", to: "Twitter", weight: 17000},
+  {from: "Google", to: "YouTube", weight: 8000},
+  {from: "Google", to: "Wikipedia", weight: 11000},
+  {from: "Bing", to: "Facebook", weight: 7500},
+  {from: "Bing", to: "Twitter", weight: 5000},
+  {from: "Bing", to: "Wikipedia", weight: 4000}
+  ];
+ //calling the Sankey function
+ var sankey_chart = anychart.sankey(data);
+ //customizing the width of the nodes
+ sankey_chart.nodeWidth("20%");
+ //setting the chart title
+ sankey_chart.title("Simple Sankey Diagram Example");
+ //customizing the vertical padding of the nodes
+ sankey_chart.nodePadding(20);
+ //setting the container id
+ sankey_chart.container("sankey");
+ //initiating drawing the Sankey diagram
+ sankey_chart.draw();
+ });
