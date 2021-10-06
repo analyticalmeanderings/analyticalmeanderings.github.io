ce740d5ce9d7a732db016779cfbb807fcd5777d6
Matthew Christian <matthew.christian@USP.org>
2021-10-05 22:18:31 -0400

    searching subsets

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  // TODO: remove the divs for build table, just build the table
   build_table('./downstream.csv', 'downstream_table');
   build_table('./upstream.csv', 'upstream_table');
   build_table('./downstream_risk.csv', 'downstream_risk');
@@ -7,7 +8,7 @@ window.onload = function() {
 }
 
 function build_top10_table() {
-  var input_table = document.getElementById('downstream_risk');
+  var input_table = document.getElementById('downstream_risk').getElementsByTagName("table");
   // var output_table = document.getElementById('top10_table');
   // output_table.innerHtml = input_table.innerHTML;
   var row = input_table.rows;
