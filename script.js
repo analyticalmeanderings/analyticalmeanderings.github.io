6a824f7cfdd39a3a40065c635fad593ca83526ea
Matthew Christian <matthew.christian@USP.org>
2021-10-06 08:13:34 -0400

    need to build numeric functionaliity

--- script.js
+++ script.js
@@ -146,11 +146,13 @@ function sortTable(n, id_target) {
       /*check if the two rows should switch place,
       based on the direction, asc or desc:*/
       if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
+        // if number ==
+        //   then run
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            //if so, mark as a switch and break the loop:
+            shouldSwitch= true;
+            break;
+          }
       } else if (dir == "desc") {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
           //if so, mark as a switch and break the loop:
