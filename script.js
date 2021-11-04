d1a9cc9a03a072a22f1fc072a64f85d1a3136b7d
Matthew Christian <2010.mbc@gmail.com>
2021-11-04 10:41:14 -0400

    adding stuff

--- script.js
+++ script.js
@@ -131,29 +131,6 @@ function build_table(input_location, id_target) {
     })
 }
 
-// function csv_string_to_table(csv_string, id_target) {
-//   var rows = csv_string.trim().split(/\r?\n|\r/); // Regex to split/separate the CSV rows
-//   var table_rows = '';
-//   var table_header = '';
-
-//   rows.forEach(function(row, row_index) {
-//       var table_columns = '';
-//       var columns = row.split(','); // split/separate the columns in a row
-//       columns.forEach(function(column, column_index) {
-//           table_columns += row_index == 0 ? '<th onclick="sortTable('+column_index.toString()+','+id_target+')">' + column + '</th>' : '<td>' + column + '</td>';
-          
-//       });
-//       if (row_index == 0) {
-//           table_header += '<tr>' + table_columns + '</tr>';
-//       } else {
-//           table_rows += '<tr>' + table_columns + '</tr>';
-//       }
-//   });
-
-//   table = '<table>' + table_header + table_rows + '</table>';
-//   document.getElementById(id_target).innerHTML = table;
-// }
-
 function sortTable(n, id_target) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
 
