25d40dcbac3a58db247bf8214cbd89314a6ac690
Matthew Christian <2010.mbc@gmail.com>
2021-12-24 09:19:57 -0500

    working on sliding

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ sitemap:
 
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) 
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
       console.log('already collapsosed')
       return
+    }
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
@@ -19,13 +20,13 @@ $(document).ready(function () {
     }
   })
 
-  if (window.location.hash && (window.location.hash == '#project' || window.location.hash =='home.html')) {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
+  // if (window.location.hash && (window.location.hash == '#project' || window.location.hash =='home.html')) {
+  //   $('.panel-cover').addClass('panel-cover--collapsed')
+  // }
 
-  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
+  // if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+  //   $('.panel-cover').addClass('panel-cover--collapsed')
+  // }
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
