30e49659d609cde8a3a13d652a1c905e5d94b374
Matt Christian <matthew.christian@usp.org>
2023-12-01 16:58:34 -0500

    fixing some, breaking otheres

--- flaming-mouse-trail.js
+++ flaming-mouse-trail.js
@@ -1,43 +1,28 @@
-const particles = [];
-const numberOfParticles = 500;
+const swirlElements = [];
+const maxElements = 10;
+let angle = 0;
 
 document.addEventListener('mousemove', (e) => {
-    if (particles.length < numberOfParticles) {
-        let particle = document.createElement('span');
-        particle.classList.add('particle', 'trail');
-        particle.style.left = e.pageX + 'px';
-        particle.style.top = e.pageY + 'px';
-        document.body.appendChild(particle);
+    if (swirlElements.length < maxElements) {
+        let element = document.createElement('div');
+        element.className = 'swirl-element';
+        document.body.appendChild(element);
+        swirlElements.push({ el: element, angle: angle, distance: 50 });
+        angle += Math.PI / 5; // determines the separation between the elements
     }
 
-    updateParticles(e);
+    swirlElements.forEach(item => {
+        const x = e.pageX + item.distance * Math.cos(item.angle);
+        const y = e.pageY + item.distance * Math.sin(item.angle);
+        item.el.style.left = `${x}px`;
+        item.el.style.top = `${y}px`;
+        item.angle += 0.05; // controls the speed of rotation
+    });
 });
 
-function updateParticles(e) {
-
-    for (let i = 0; i < particles.length; i++) {
-        let particle = particles[i];
-        if (!particle) continue;
-
-        if (i === 0) {
-            particle.style.left = e.pageX + 'px';
-            particle.style.top = e.pageY + 'px';
-        } else {
-            let prevParticle = particles[i - 1];
-            particle.style.left = prevParticle.style.left;
-            particle.style.top = prevParticle.style.top;
-        }
-
-        // Fade out and remove old particles
-        if (i === particles.length - 1) {
-            let opacity = parseFloat(particle.style.opacity);
-            if (opacity <= 0) {
-                document.body.removeChild(particle);
-                particles.splice(i, 1);
-                i--;
-            } else {
-                particle.style.opacity = opacity - 0.1;
-            }
-        }
+setInterval(() => {
+    if (swirlElements.length > maxElements) {
+        const element = swirlElements.shift().el;
+        element.remove();
     }
-}
+}, 100);
